e61caa742e3390f05c4c3e6d7e71fdbf32d46ae1
Sharath <sharathkrml@gmail.com>
2022-08-25 12:47:18 +0530

    figured out assetClass null

--- src/exchange-v-2.ts
+++ src/exchange-v-2.ts
@@ -1,6 +1,6 @@
 import { Match as MatchEvent } from "../generated/ExchangeV2/ExchangeV2";
 import { Match } from "../generated/schema";
-import { ethereum, TypedMap } from "@graphprotocol/graph-ts";
+import { ByteArray, Bytes, ethereum, TypedMap } from "@graphprotocol/graph-ts";
 export const classMap = new TypedMap<string, string>();
 classMap.set("0xaaaebeba", "ETH");
 classMap.set("0x8ae85d84", "ERC20");
@@ -29,11 +29,7 @@ export function handleMatch(event: MatchEvent): void {
       let decodedTuple = decoded.toTuple();
       entity.leftAddress = decodedTuple[0].toAddress();
     }
-  } else if (
-    entity.leftClass == "ERC721" ||
-    entity.leftClass == "ERC1155" ||
-    entity.leftClass == null
-  ) {
+  } else if (entity.leftClass == "ERC721" || entity.leftClass == "ERC1155") {
     let decoded = ethereum.decode(
       "(address,uint256)",
       event.params.leftAsset.data
@@ -43,6 +39,26 @@ export function handleMatch(event: MatchEvent): void {
       entity.leftAddress = decodedTuple[0].toAddress();
       entity.leftId = decodedTuple[1].toBigInt();
     }
+  } else if (entity.leftClass == null) {
+    let decoded = ethereum.decode(
+      "(address,uint256,uint256)",
+      event.params.leftAsset.data
+    );
+    if (decoded) {
+      let decodedTuple = decoded.toTuple();
+      entity.leftAddress = decodedTuple[0].toAddress();
+      entity.leftId = decodedTuple[2].toBigInt();
+    }
+    // (id) = abi.decode(data[64:128],(uint256));
+    // if (entity.leftAssetdata!.length > 194) {
+    //   let idString = "0x" + entity.leftAssetdata!.slice(130, 194);
+    //   let idBytes = Bytes.fromHexString(idString);
+    //   let decodedId = ethereum.decode("(uint256)", idBytes);
+    //   if (decodedId) {
+    //     let decodedTuple = decodedId.toTuple();
+    //     entity.leftId = decodedTuple[0].toBigInt();
+    //   }
+    // }
   }
   entity.leftAssetdata = event.params.leftAsset.data.toHexString();
   entity.rightClass = classMap.get(
