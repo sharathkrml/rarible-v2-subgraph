4fc710ae4be2d47f8a5b9c063cf004a933dcfcd9
Sharath <sharathkrml@gmail.com>
2022-08-25 15:14:41 +0530

    completed match Entity

--- src/exchange-v-2.ts
+++ src/exchange-v-2.ts
@@ -1,13 +1,14 @@
 import { Match as MatchEvent } from "../generated/ExchangeV2/ExchangeV2";
 import { Match } from "../generated/schema";
 import { ByteArray, Bytes, ethereum, TypedMap } from "@graphprotocol/graph-ts";
-export const classMap = new TypedMap<string, string>();
-classMap.set("0xaaaebeba", "ETH");
-classMap.set("0x8ae85d84", "ERC20");
-classMap.set("0x73ad2146", "ERC721");
-classMap.set("0x973bb640", "ERC1155");
-classMap.set("0xf63c2825", "COLLECTION");
-classMap.set("0x3e6b89d4", "CRYPTOPUNKS");
+import {
+  classMap,
+  ETH,
+  ERC20,
+  ERC721,
+  ERC1155,
+  getClass,
+} from "./rarible-helper";
 export function handleMatch(event: MatchEvent): void {
   let entity = Match.load(event.transaction.hash.toHexString());
   if (!entity) {
@@ -20,16 +21,14 @@ export function handleMatch(event: MatchEvent): void {
   entity.newLeftFill = event.params.newLeftFill;
   entity.newRightFill = event.params.newRightFill;
   // entity.leftAssetClass = event.params.leftAsset.assetClass.toHexString();
-  entity.leftClass = classMap.get(
-    event.params.leftAsset.assetClass.toHexString()
-  );
-  if (entity.leftClass == "ERC20") {
+  entity.leftClass = getClass(event.params.leftAsset.assetClass);
+  if (entity.leftClass == ERC20) {
     let decoded = ethereum.decode("(address)", event.params.leftAsset.data);
     if (decoded) {
       let decodedTuple = decoded.toTuple();
       entity.leftAddress = decodedTuple[0].toAddress();
     }
-  } else if (entity.leftClass == "ERC721" || entity.leftClass == "ERC1155") {
+  } else if (entity.leftClass == ERC721 || entity.leftClass == ERC1155) {
     let decoded = ethereum.decode(
       "(address,uint256)",
       event.params.leftAsset.data
@@ -61,10 +60,46 @@ export function handleMatch(event: MatchEvent): void {
     // }
   }
   entity.leftAssetdata = event.params.leftAsset.data.toHexString();
-  entity.rightClass = classMap.get(
-    event.params.rightAsset.assetClass.toHexString()
-  );
+  entity.rightClass = getClass(event.params.rightAsset.assetClass);
+
   // entity.rightAssetClass = event.params.rightAsset.assetClass.toHexString();
+  if (entity.rightClass == ERC20) {
+    let decoded = ethereum.decode("(address)", event.params.rightAsset.data);
+    if (decoded) {
+      let decodedTuple = decoded.toTuple();
+      entity.rightAddress = decodedTuple[0].toAddress();
+    }
+  } else if (entity.rightClass == ERC721 || entity.rightClass == ERC1155) {
+    let decoded = ethereum.decode(
+      "(address,uint256)",
+      event.params.rightAsset.data
+    );
+    if (decoded) {
+      let decodedTuple = decoded.toTuple();
+      entity.rightAddress = decodedTuple[0].toAddress();
+      entity.rightId = decodedTuple[1].toBigInt();
+    }
+  } else if (entity.rightClass == null) {
+    let decoded = ethereum.decode(
+      "(address,uint256,uint256)",
+      event.params.rightAsset.data
+    );
+    if (decoded) {
+      let decodedTuple = decoded.toTuple();
+      entity.rightAddress = decodedTuple[0].toAddress();
+      entity.rightId = decodedTuple[2].toBigInt();
+    }
+    // (id) = abi.decode(data[64:128],(uint256));
+    // if (entity.leftAssetdata!.length > 194) {
+    //   let idString = "0x" + entity.leftAssetdata!.slice(130, 194);
+    //   let idBytes = Bytes.fromHexString(idString);
+    //   let decodedId = ethereum.decode("(uint256)", idBytes);
+    //   if (decodedId) {
+    //     let decodedTuple = decodedId.toTuple();
+    //     entity.leftId = decodedTuple[0].toBigInt();
+    //   }
+    // }
+  }
   entity.rightAssetdata = event.params.rightAsset.data.toHexString();
 
   entity.save();
