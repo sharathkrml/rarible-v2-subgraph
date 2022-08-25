6a3e0d30c311e42b70d408125685e214e778ffc0
Sharath <sharathkrml@gmail.com>
2022-08-25 23:30:03 +0530

    added decodeAsset Helper function

--- src/exchange-v-2.ts
+++ src/exchange-v-2.ts
@@ -8,6 +8,8 @@ import {
   ERC721,
   ERC1155,
   getClass,
+  decodeAsset,
+  SPECIAL,
 } from "./rarible-helper";
 export function handleMatch(event: MatchEvent): void {
   let entity = Match.load(event.transaction.hash.toHexString());
@@ -18,66 +20,17 @@ export function handleMatch(event: MatchEvent): void {
   entity.rightMaker = event.params.rightMaker.toHexString();
   entity.newLeftFill = event.params.newLeftFill;
   entity.newRightFill = event.params.newRightFill;
-  // entity.leftAssetClass = event.params.leftAsset.assetClass.toHexString();
+  //  Light
   entity.leftClass = getClass(event.params.leftAsset.assetClass);
-  if (entity.leftClass == ERC20) {
-    let decoded = ethereum.decode("(address)", event.params.leftAsset.data);
-    if (decoded) {
-      let decodedTuple = decoded.toTuple();
-      entity.leftAddress = decodedTuple[0].toAddress();
-    }
-  } else if (entity.leftClass == ERC721 || entity.leftClass == ERC1155) {
-    let decoded = ethereum.decode(
-      "(address,uint256)",
-      event.params.leftAsset.data
-    );
-    if (decoded) {
-      let decodedTuple = decoded.toTuple();
-      entity.leftAddress = decodedTuple[0].toAddress();
-      entity.leftId = decodedTuple[1].toBigInt();
-    }
-  } else if (entity.leftClass == null) {
-    let decoded = ethereum.decode(
-      "(address,uint256,uint256)",
-      event.params.leftAsset.data
-    );
-    if (decoded) {
-      let decodedTuple = decoded.toTuple();
-      entity.leftAddress = decodedTuple[0].toAddress();
-      entity.leftId = decodedTuple[2].toBigInt();
-    }
-  }
+  let asset = decodeAsset(event.params.leftAsset.data, entity.leftClass!);
+  entity.leftAddress = asset.address;
+  entity.leftId = asset.id;
   entity.leftAssetdata = event.params.leftAsset.data.toHexString();
+  //  Right
   entity.rightClass = getClass(event.params.rightAsset.assetClass);
-
-  // entity.rightAssetClass = event.params.rightAsset.assetClass.toHexString();
-  if (entity.rightClass == ERC20) {
-    let decoded = ethereum.decode("(address)", event.params.rightAsset.data);
-    if (decoded) {
-      let decodedTuple = decoded.toTuple();
-      entity.rightAddress = decodedTuple[0].toAddress();
-    }
-  } else if (entity.rightClass == ERC721 || entity.rightClass == ERC1155) {
-    let decoded = ethereum.decode(
-      "(address,uint256)",
-      event.params.rightAsset.data
-    );
-    if (decoded) {
-      let decodedTuple = decoded.toTuple();
-      entity.rightAddress = decodedTuple[0].toAddress();
-      entity.rightId = decodedTuple[1].toBigInt();
-    }
-  } else if (entity.rightClass == null) {
-    let decoded = ethereum.decode(
-      "(address,uint256,uint256)",
-      event.params.rightAsset.data
-    );
-    if (decoded) {
-      let decodedTuple = decoded.toTuple();
-      entity.rightAddress = decodedTuple[0].toAddress();
-      entity.rightId = decodedTuple[2].toBigInt();
-    }
-  }
+  asset = decodeAsset(event.params.rightAsset.data, entity.rightClass!);
+  entity.rightAddress = asset.address;
+  entity.rightId = asset.id;
   entity.rightAssetdata = event.params.rightAsset.data.toHexString();
 
   entity.save();
--- src/rarible-helper.ts
+++ src/rarible-helper.ts
@@ -11,6 +11,7 @@ export const ERC721 = "ERC721";
 export const ERC1155 = "ERC1155";
 export const COLLECTION = "COLLECTION";
 export const CRYPTOPUNKS = "CRYPTOPUNKS";
+export const SPECIAL = "SPECIAL";
 export const classMap = new TypedMap<string, string>();
 classMap.set("0xaaaebeba", ETH);
 classMap.set("0x8ae85d84", ERC20);
@@ -18,38 +19,51 @@ classMap.set("0x73ad2146", ERC721);
 classMap.set("0x973bb640", ERC1155);
 classMap.set("0xf63c2825", COLLECTION);
 classMap.set("0x3e6b89d4", CRYPTOPUNKS);
-export function getClass(assetClass: Bytes): string | null {
-  return classMap.get(assetClass.toHexString());
-}
-export function getERC20Address(data: Bytes): Address {
-  let decoded = ethereum.decode("(address)", data);
-  if (decoded) {
-    let decodedTuple = decoded.toTuple();
-    return decodedTuple[0].toAddress();
+export function getClass(assetClass: Bytes): string {
+  let res = classMap.get(assetClass.toHexString());
+  if (res) {
+    return res;
   }
-  return Address.fromString("0x0000000000000000000000000000000000000000");
+  return SPECIAL;
 }
-export function getNFTAddress(data: Bytes): Address {
-  let decoded = ethereum.decode("(address,uint256)", data);
-  if (decoded) {
-    let decodedTuple = decoded.toTuple();
-    return decodedTuple[0].toAddress();
+export class Asset {
+  address: Address;
+  id: BigInt;
+  constructor(address: Address, id: BigInt) {
+    this.address = address;
+    this.id = id;
   }
-  return Address.fromString("0x0000000000000000000000000000000000000000");
 }
-export function getNFTId(data: Bytes): BigInt {
-  let decoded = ethereum.decode("(address,uint256)", data);
-  if (decoded) {
-    let decodedTuple = decoded.toTuple();
-    return decodedTuple[1].toBigInt();
+export function decodeAsset(data: Bytes, type: String): Asset {
+  if (type == ERC20) {
+    let decoded = ethereum.decode("(address)", data);
+    if (decoded) {
+      let decodedTuple = decoded.toTuple();
+      let asset = new Asset(decodedTuple[0].toAddress(), BigInt.fromI32(0));
+      return asset;
+    }
+  } else if (type == ERC721 || type == ERC1155) {
+    let decoded = ethereum.decode("(address,uint256)", data);
+    if (decoded) {
+      let decodedTuple = decoded.toTuple();
+      let address = decodedTuple[0].toAddress();
+      let id = decodedTuple[1].toBigInt();
+      let asset = new Asset(address, id);
+      return asset;
+    }
+  } else if (type == SPECIAL) {
+    let decoded = ethereum.decode("(address,uint256,uint256)", data);
+    if (decoded) {
+      let decodedTuple = decoded.toTuple();
+      let address = decodedTuple[0].toAddress();
+      let id = decodedTuple[2].toBigInt();
+      let asset = new Asset(address, id);
+      return asset;
+    }
   }
-  return BigInt.fromI32(0);
-}
-export function getNullNFTId(data: Bytes): BigInt {
-  let decoded = ethereum.decode("(address,uint256,uint256)", data);
-  if (decoded) {
-    let decodedTuple = decoded.toTuple();
-    return decodedTuple[2].toBigInt();
-  }
-  return BigInt.fromI32(0);
+  let asset = new Asset(
+    Address.fromString("0x0000000000000000000000000000000000000000"),
+    BigInt.fromI32(0)
+  );
+  return asset;
 }
